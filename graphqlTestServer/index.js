04ec6a1f733992014f11c71f8f466ecd6d45021d
JakobJK <jakobjk@gmail.com>
2019-07-25 14:06:48 -0400

    adding in the parser

--- graphqlTestServer/index.js
+++ graphqlTestServer/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
-const parser = require('../Parser/queryParser/.js');
+const parser = require('../Parser/queryParser');
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
-app.use('/graphql', (req, res, next) => {
-  console.log(req.body.query);
-  return next();
-}, graphqlHTTP({
+app.use('/graphql', (req, res, next) =>
+  // if (req.body.query !== undefined) {
+  //   console.log(parser(req.body.query));
+  // }
+  next(),
+graphqlHTTP({
   schema,
   graphiql: true,
 }));
