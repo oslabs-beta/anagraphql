0ebaf47e6253494882fec0345f54518c07ce66b5
Jacky <jackychow328@gmail.com>
2019-07-26 16:51:24 -0400

    created Editor component

--- anagraphqlEditor/src/client/components/CodeEditor.jsx
+++ anagraphqlEditor/src/client/components/CodeEditor.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+const CodeMirrorComponent = require('react-codemirror');
+
+class CodeEditor extends React.Component {
+    render() {
+        return (
+            <CodeMirrorComponent/>
+        )
+    }
+}
+
+export default CodeEditor;
