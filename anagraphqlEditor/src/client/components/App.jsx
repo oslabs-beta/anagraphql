6fd7a3d309051d208f2d624bd2e28fcfb4a38d12
Alfredo Alpizar <Fredo@Dipnas-MacBook.local>
2019-07-26 10:26:58 -0400

    set up frontend dev environment

---
+++ anagraphqlEditor/src/client/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import MainContainer from "./containers/MainContainer.jsx";
+const App = () => (
+  <div id="App">
+      <h1>test</h1>
+  </div>
+);
+
+export default App;
