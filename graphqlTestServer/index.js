372ecfe69f090745c872c7963ce6d985e2251171
JakobJK <jakobjk@gmail.com>
2019-07-27 15:21:03 -0400

    Using the anagraphql middleware on testserver

--- graphqlTestServer/index.js
+++ graphqlTestServer/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
+const anagraphql = require('anagraphql');
 const schema = require('./schema');
 const parser = require('../Parser/queryParser');
 
@@ -8,17 +9,11 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
-app.use('/graphql', (req, res, next) => {
-  // if (req.body.query !== undefined) {
-  //   console.log(JSON.stringify(parser(req.body.query)));
-  // }
-
-  next();
-},
-graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
+app.use('/graphql', anagraphql({ graphiql: true, schema }),
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }));
 
 
 app.listen(port, () => (console.log(`something on port ${port}`)));
