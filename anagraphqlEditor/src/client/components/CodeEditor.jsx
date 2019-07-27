6922072c04fa32c36f7d94d4d46ec848f066f94c
Jacky <jackychow328@gmail.com>
2019-07-26 20:23:45 -0400

    updated code editor to have syntax highlighting with graphql

--- anagraphqlEditor/src/client/components/CodeEditor.jsx
+++ anagraphqlEditor/src/client/components/CodeEditor.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 const CodeMirrorComponent = require('react-codemirror');
+require('../../../dist/mode/javascript/javascript');
+require('codemirror/mode/javascript/javascript');
+require('../../../dist/lib/night.css');
+// import 'codemirror/addon/hint/show-hint';
+// import 'codemirror/addon/lint/lint';
+// import 'codemirror-graphql/hint';
+// import 'codemirror-graphql/lint';
+require('../../../dist/lib/mode.js');
+
 
 const CodeEditor = () => {
     const [code, updateCode] = useState('');
     return (
         <div>
-            <CodeMirrorComponent value={code} onChange={updateCode} options={{lineNumbers: true, theme: "night"}}/>
+            <CodeMirrorComponent value={code} onChange={updateCode} options={{lineNumbers: true, mode: 'graphql', theme: 'night'}}/>
             <p>{code}</p>
         </div>
     )
