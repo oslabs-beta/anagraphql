df3ed4f4a2bd5db6f6a914debff4dbe75fe4c209
Alfredo Alpizar <Fredo@Dipnas-MacBook.local>
2019-07-27 14:46:45 -0400

    passing schema to frontend from function that renders html

--- renderGraphiql.js
+++ renderGraphiql.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const fs = require('fs');
-module.exports = ()=>{
-    const content = fs.readFileSync(path.join(__dirname,'bundle.js'), 'utf-8');
-    return `<!DOCTYPE html>
+
+
+module.exports = (schema) => {
+  const content = fs.readFileSync(path.join(__dirname, 'bundle.js'), 'utf-8');
+  return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,7 +14,9 @@ module.exports = ()=>{
 </head>
 <body>
     <div id="root"></div>
+    <script>const schema = ${JSON.stringify(schema)};</script>
+
     <script>${content}</script>
 </body>
-</html>`
-}
+</html>`;
+};
