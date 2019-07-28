1914662686e63b4b4e4fb1fa05653c4b6cc5b1f1
Alfredo Alpizar <Fredo@Dipnas-MacBook.local>
2019-07-27 23:02:18 -0400

    imported and implemented anagraphql

--- graphqlTestServer/index.js
+++ graphqlTestServer/index.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
+const anagraphql = require('anagraphql');
 const schema = require('./schema');
-const parser = require('../Parser/queryParser');
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
-
-app.use('/graphql', (req, res, next) => {
-  // if (req.body.query !== undefined) {
-  //   console.log(JSON.stringify(parser(req.body.query)));
-  // }
-
-  next();
-},
-graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
+console.log(schema.getQueryType)
+app.use('/graphql', anagraphql({schema, graphiql:true}));
 
 
 app.listen(port, () => (console.log(`something on port ${port}`)));
