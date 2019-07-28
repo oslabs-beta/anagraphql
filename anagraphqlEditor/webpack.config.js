d728f6117021dc5cfded32f121414fb37ff06e57
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-28 19:00:59 -0400

    formatted & added resolve for extensions .js .jsx

--- anagraphqlEditor/webpack.config.js
+++ anagraphqlEditor/webpack.config.js
@@ -1,36 +1,39 @@
 const path = require('path');
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: path.join(__dirname, 'src', 'client', 'index.js' ),
-    output: {
-        publicPath: path.join(__dirname, '..'),
-        path: path.join(__dirname, '..'),
-        filename: 'bundle.js'
+  mode: process.env.NODE_ENV,
+  entry: path.join(__dirname, 'src', 'client', 'index.jsx'),
+  output: {
+    publicPath: path.join(__dirname, '..'),
+    path: path.join(__dirname, '..'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    proxy: {
+      '/api': 'http://localhost:3000',
     },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options:{
-                    presets: ['@babel/preset-env', '@babel/preset-react']
-                }
-            },
-            {
-                test: /\.s?css$/,
-                exclude: /node_modules/,
-                loaders: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        publicPath: '/dist/',
-        proxy: {
-            '/api':"http://localhost:3000"
-        }
 
-    }
-}
+  },
+};
