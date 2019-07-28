88c53c0a61c21d51230cdcc322b01b5eb776c7ad
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-28 17:32:30 -0400

    fixing eslint errors

--- anagraphqlEditor/src/client/index.js
+++
@@ -1,6 +0,0 @@
-import {render} from 'react-dom';
-import React from 'react';
-// import { Provider } from "react-redux";
-import App from './components/App.jsx'
-
-render( <App/>, document.getElementById('root'))
