60b8b93e939b179309588777d0834d7ed2be5e91
Alfredo Alpizar <Fredo@Dipnas-MacBook.local>
2019-07-27 23:24:10 -0400

    removed console log

--- graphqlTestServer/index.js
+++ graphqlTestServer/index.js
@@ -7,7 +7,6 @@ const schema = require('./schema');
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
-console.log(schema.getQueryType)
 app.use('/graphql', anagraphql({ schema, graphiql: true }));
 
 
