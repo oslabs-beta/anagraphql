5ed7d288f9ac92160826e4eeb4c533058c745f1d
Alfredo Alpizar <Fredo@Dipnas-MacBook.local>
2019-07-27 22:55:02 -0400

    codemirror files in client/utility. clean imports

--- anagraphqlEditor/src/client/components/CodeEditor.jsx
+++ anagraphqlEditor/src/client/components/CodeEditor.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 const CodeMirrorComponent = require('react-codemirror');
-require('../../../dist/mode/javascript/javascript');
-require('codemirror/mode/javascript/javascript');
-require('../../../dist/lib/night.css');
+import '../utility/codemirror/codemirror.js'
+import '../utility/codemirror/codemirror.css'
+import'../utility/codemirror/mode.js';
 // import 'codemirror/addon/hint/show-hint';
 // import 'codemirror/addon/lint/lint';
 // import 'codemirror-graphql/hint';
 // import 'codemirror-graphql/lint';
-require('../../../dist/lib/mode.js');
 
 
 const CodeEditor = () => {
     const [code, updateCode] = useState('');
     return (
         <div>
-            <CodeMirrorComponent value={code} onChange={updateCode} options={{lineNumbers: true, mode: 'graphql', theme: 'night'}}/>
+            <CodeMirrorComponent value={code} onChange={updateCode} options={{lineNumbers: true, mode: 'graphql'}}/>
             <p>{code}</p>
         </div>
     )
