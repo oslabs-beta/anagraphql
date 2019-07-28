f9d723b09be879cce5333fdddcede0c8a5f8ef60
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-28 19:01:31 -0400

    Implementing redux: added Provider wrapper & state

--- anagraphqlEditor/src/client/index.jsx
+++ anagraphqlEditor/src/client/index.jsx
@@ -1,6 +1,10 @@
 import { render } from 'react-dom';
 import React from 'react';
-// import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store';
 
-render(<App />, document.getElementById('root'));
+
+render(
+  <Provider store={store}><App /></Provider>, document.getElementById('root'),
+);
