5b09ef3c36eb01e6c93921c5f73ac34268964157
JakobJK <jakobjk@gmail.com>
2019-07-29 09:39:15 -0400

    adding the anagraphql object to res.locals

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const parser = require('./Parser/queryParser');
 const renderGraphiql = require('./renderGraphiql');
-
+const anagraphCreator = require('./Parser/anagraphCreator');
 /**
  * Function that returns express middleware.
  * It accepts an options object that configures it's behavior.
  */
-const anagraphql = options => ((req, res) => {
+const anagraphql = options => ((req, res, next) => {
   if (req.method !== 'GET' && req.method !== 'POST') {
     res.setHeader('Allow', 'GET, POST');
     res.status(405).send('GraphQL only supports GET and POST requests.');
@@ -16,8 +16,18 @@ const anagraphql = options => ((req, res) => {
   if (!schema) throw new Error('GraphQL middleware options must contain a schema.');
   // const {query} = req.body;
 
+  if (req.body.query) {
+    res.locals.anagraph = anagraphCreator(req.body.query);
+  }
+
   if (graphiql && req.method === 'GET') {
     res.send(renderGraphiql());
+    res.end();
+  }
+
+
+  if (!graphiql) {
+    return next();
   }
 });
 
