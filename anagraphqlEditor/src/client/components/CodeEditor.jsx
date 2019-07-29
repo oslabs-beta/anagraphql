6e9dcc5c1c4f0f81152663acb60c2bff35f95aac
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-29 14:07:12 -0400

    changed codemirror options&implemented redux hooks

--- anagraphqlEditor/src/client/components/CodeEditor.jsx
+++ anagraphqlEditor/src/client/components/CodeEditor.jsx
@@ -1,21 +1,62 @@
-import React, { useState } from 'react';
-import '../utility/codemirror/codemirror.js';
-import '../utility/codemirror/codemirror.css';
-import '../utility/codemirror/mode.js';
+import React from 'react';
+import CodeMirrorComponent from 'react-codemirror';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateQuery } from '../actions/actions';
 
-const CodeMirrorComponent = require('react-codemirror');
-// import 'codemirror/addon/hint/show-hint';
-// import 'codemirror/addon/lint/lint';
-// import 'codemirror-graphql/hint';
-// import 'codemirror-graphql/lint';
+import 'codemirror/lib/codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/search/search';
+import 'codemirror/addon/search/jump-to-line';
+import 'codemirror/addon/dialog/dialog';
+import 'codemirror/keymap/sublime';
+import 'codemirror/addon/lint/lint';
+import 'codemirror-graphql/hint';
+import 'codemirror-graphql/lint';
+import 'codemirror-graphql/info';
+import 'codemirror-graphql/jump';
+import 'codemirror-graphql/mode';
 
 
 const CodeEditor = () => {
-  const [code, updateCode] = useState('');
+  const { query, schema } = useSelector(state => ({
+    query: state.query.query,
+    schema: state.query.schema,
+  }));
+  const dispatch = useDispatch();
+
+  const options = {
+    lineNumbers: true,
+    tabSize: 2,
+    keyMap: 'sublime',
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    showCursorWhenSelecting: true,
+    foldGutter: {
+      minFoldSize: 4,
+    },
+    mode: 'graphql',
+    lint: {
+      schema,
+    },
+    hintOptions: {
+      schema,
+    },
+  };
+
   return (
     <div>
-      <CodeMirrorComponent value={code} onChange={updateCode} options={{ lineNumbers: true, mode: 'graphql' }} />
-      <p>{code}</p>
+      <CodeMirrorComponent
+        value={query}
+        onChange={currQ => dispatch(updateQuery(currQ))}
+        options={options}
+      />
+      {/* <button onClick={() => dispatch()}>Send Query</button> */}
     </div>
   );
 };
