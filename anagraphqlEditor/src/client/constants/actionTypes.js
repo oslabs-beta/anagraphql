4ff5814ac2d537bcaf9b6a58b6ca0f9908a036d9
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-29 14:13:55 -0400

    fixed GET_SCHEMA action type

--- anagraphqlEditor/src/client/constants/actionTypes.js
+++ anagraphqlEditor/src/client/constants/actionTypes.js
@@ -2,4 +2,4 @@ export const UPDATE_QUERY = 'UPDATE_QUERY';
 export const ADD_SAVED_QUERY = 'ADD_SAVED_QUERY';
 export const DELETE_SAVED_QUERY = 'DELETE_SAVED_QUERY';
 export const GET_QUERY_RESPONSE = 'GET_QUERY_RESPONSE';
-export const GET_SCHEMA = 'BUILD_SCHEMA';
+export const GET_SCHEMA = 'GET_SCHEMA';
