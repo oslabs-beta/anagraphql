c5d4af01d789c300ccd81d019a4f5d2c27c18839
JakobJK <jakobjk@gmail.com>
2019-07-29 09:41:48 -0400

    file which creates our anagraph object

---
+++ Parser/anagraphCreator.js
@@ -0,0 +1,11 @@
+const parser = require('./queryParser');
+const validator = require('./stringValidator');
+const stringProperties = require('./stringProperties');
+
+module.exports = (query) => {
+  const theOBJ = parser(query);
+  const anagraph = {};
+  anagraph.analytics = stringProperties.countResolvers(theOBJ);
+  anagraph.analytics.maxNested = stringProperties.countDepth(query);
+  return anagraph;
+};
--- renderGraphiql.js
+++ renderGraphiql.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-module.exports = ()=>{
-    const content = fs.readFileSync(path.join(__dirname,'bundle.js'), 'utf-8');
-    return `<!DOCTYPE html>
+
+module.exports = () => {
+  const content = fs.readFileSync(path.join(__dirname, 'bundle.js'), 'utf-8');
+  return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -14,5 +15,5 @@ module.exports = ()=>{
     <div id="root"></div>
     <script>${content}</script>
 </body>
-</html>`
-}
+</html>`;
+};
