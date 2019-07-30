5e0701411e0d3b42e52eaa591903b8c1bb84378c
Camera <camerawalrond@gmail.com>
2019-07-30 11:32:44 -0400

    added comments to actionTypes, queryReducer, and responseReducer files

--- anagraphqlEditor/src/client/reducers/responseReducer.js
+++ anagraphqlEditor/src/client/reducers/responseReducer.js
@@ -1,9 +1,13 @@
 import { GET_QUERY_RESPONSE } from '../constants/actionTypes';
 
+
+// Sets variables for initial response store
 const initialState = {
   currResponse: null,
   responseList: [],
 };
+
+//The responseReducer function returns either a default response (state) or, if the GET_QUERY_RESPONSE action type is passed as an argument, returns the current response and response list.
 
 const responseReducer = (state = initialState, action) => {
   switch (action.type) {
