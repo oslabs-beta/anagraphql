3899b63797a8a66449846fa68f892fedf3f09f89
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-29 20:15:26 -0400

    added ResponseViewer.jsx

--- anagraphqlEditor/src/client/components/App.jsx
+++ anagraphqlEditor/src/client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CodeEditor from './CodeEditor';
+import ResponseViewer from './ResponseViewer';
 // import MainContainer from "./containers/MainContainer.jsx";
 const App = () => (
   <div id="App">
     <h1>Test</h1>
     <CodeEditor />
+    <ResponseViewer />
   </div>
 );
 
