beaaa8181f110622db3b117eab0710d6e4fd260f
Jacky <jackychow328@gmail.com>
2019-07-30 18:20:28 -0400

    uploaded logo to app.jsx

--- anagraphqlEditor/src/client/components/App.jsx
+++ anagraphqlEditor/src/client/components/App.jsx
@@ -11,35 +11,35 @@ const routes = [
   {
     path: '/',
     exact: true,
-    sidebar: () => <div>Code Editor</div>,
+    sidebar: () => <div>Code Editor!</div>,
     main: () => <CodeEditor/>
   },
   {
     path: '/about',
     exact: true,
     sidebar: () => <div>About!</div>,
-    main: () => <h2>YYOOOJLKFLJLFJDSOIFJSOIFLKSDFJFOSIJFLKJFDLJFISFLSDK</h2>
+    main: () => <h2>This will be our ReadMe file</h2>
   },
   {
     path: '/policies',
     exact: true,
     sidebar: () => <div>Policies!</div>,
-    main: () => <h2>LAKJFLKDSJFLSKJFLSKJFLSKJFLSJFLSJFSLKJFLSJD</h2>
+    main: () => <h2>This will be our policies component</h2>
   },
 ]
 
 const App = () => (
   <div id="App">
     <div id="title">
-      <h1>AnagraphQL</h1>
+      <img src="./IMG_0672.jpg" alt="Anagraph Logo" height="300px"></img>
     </div>
     <Router>
       <div style={{display: 'flex'}}>
         <div style={{
           padding: '10px',
-          background: '#f0f0f0',
+          backgroundColor: '#ff8dd9',
         }}>
-          <ul style={{listStyleType: 'none', padding: 0}}>
+          <ul style={{listStyleType: 'none', padding: 0, height: '500px', width: '200px'}}>
             <li><Link to='/'>Code Editor</Link></li>
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/policies'>Policies</Link></li>
