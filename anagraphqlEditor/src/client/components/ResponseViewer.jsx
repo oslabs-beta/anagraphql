fedeff7eaf914601d8adc2be5b549bbcdfded55d
Jake Kousholt <jakobjk@gmail.com>
2019-07-29 20:20:50 -0400

    Update ResponseViewer.jsx
    Removed unnecessary imports

--- anagraphqlEditor/src/client/components/ResponseViewer.jsx
+++ anagraphqlEditor/src/client/components/ResponseViewer.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import 'codemirror/addon/fold/foldgutter';
-import 'codemirror/addon/dialog/dialog';
-import 'codemirror/addon/search/search';
-import 'codemirror/addon/search/searchcursor';
-import 'codemirror/addon/search/jump-to-line';
-import 'codemirror/keymap/sublime';
-import 'codemirror-graphql/results/mode';
-import 'codemirror/addon/fold/brace-fold';
 
 const ResponseViewer = () => {
   const response = useSelector(state => state.response.currResponse);
