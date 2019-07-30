8668fecb91330ebc2606e02786a25e8c98ac4b54
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-30 16:51:17 -0400

    creates anagraph on POST, button disable on errors

--- anagraphqlEditor/src/client/components/CodeEditor.jsx
+++ anagraphqlEditor/src/client/components/CodeEditor.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateQuery, getQueryResponse } from '../actions/actions';
+import { updateQuery, getQueryResponse, createAnagraph } from '../actions/actions';
+import anagraphCreator from '../utility/anagraphCreator';
 
 import 'codemirror/lib/codemirror';
 import 'codemirror/lib/codemirror.css';
@@ -26,12 +27,14 @@ import 'codemirror-graphql/mode';
 
 
 const CodeEditor = () => {
+  const AUTO_COMPLETE_AFTER_KEY = /^[a-zA-Z0-9_@(]$/;
+
   const { query, schema } = useSelector(state => ({
     query: state.query.query,
     schema: state.query.schema,
   }));
+  const [hasErrors, setErrors] = useState(true);
   const dispatch = useDispatch();
-
   const options = {
     lineNumbers: true,
     tabSize: 2,
@@ -49,17 +52,31 @@ const CodeEditor = () => {
     },
     hintOptions: {
       schema,
+      closeOnUnfocus: false,
+      completeSingle: false,
     },
+  };
+
+  const handleQuery = () => {
+    dispatch(getQueryResponse(query));
+    const anagraph = anagraphCreator(query);
+    dispatch(createAnagraph(anagraph));
   };
 
   return (
     <div>
       <CodeMirror
         value={query}
+        onKeyUp={(editor, event) => {
+          if (AUTO_COMPLETE_AFTER_KEY.test(event.key)) {
+            editor.execCommand('autocomplete');
+          }
+        }}
         onBeforeChange={(editor, data, value) => dispatch(updateQuery(value))}
+        onUpdate={editor => setErrors(editor.state.lint.marked.length !== 0)}
         options={options}
       />
-      <button type="button" onClick={() => dispatch(getQueryResponse(query))}>Send Query</button>
+      <button type="button" onClick={handleQuery} disabled={hasErrors}>Send Query</button>
     </div>
   );
 };
