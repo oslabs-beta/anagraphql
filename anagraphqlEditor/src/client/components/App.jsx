a3a7881811543f1524c8d6b5c558c4c06dcce843
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-30 11:10:06 -0400

    added History component

--- anagraphqlEditor/src/client/components/App.jsx
+++ anagraphqlEditor/src/client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CodeEditor from './CodeEditor';
 import ResponseViewer from './ResponseViewer';
+import History from './History';
 // import MainContainer from "./containers/MainContainer.jsx";
 const App = () => (
   <div id="App">
     <h1>Test</h1>
     <CodeEditor />
     <ResponseViewer />
+    <History />
   </div>
 );
 
