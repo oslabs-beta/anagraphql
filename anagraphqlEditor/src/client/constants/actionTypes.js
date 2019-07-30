fe2f1fe28c1a0805a7eb46ac0a3ca8bd7b594e26
Camera <camerawalrond@gmail.com>
2019-07-30 12:19:45 -0400

    added more comments

--- anagraphqlEditor/src/client/constants/actionTypes.js
+++ anagraphqlEditor/src/client/constants/actionTypes.js
@@ -1,5 +1,6 @@
 //Describes actions needed for redux reducers
 //Basically CRUD actions for interacting with query state on the frontend
+//The functionality for these actionTypes are in the action file
 
 export const UPDATE_QUERY = 'UPDATE_QUERY';
 export const ADD_SAVED_QUERY = 'ADD_SAVED_QUERY';
