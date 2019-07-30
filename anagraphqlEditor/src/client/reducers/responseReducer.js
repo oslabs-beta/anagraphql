043ddfb35dc1ad36d96fd5e5d93e53cdf4de96aa
Alfredo Alpizar <fredo@Alfredos-MacBook-Pro.local>
2019-07-30 16:52:03 -0400

    handled anagraph action types

--- anagraphqlEditor/src/client/reducers/responseReducer.js
+++ anagraphqlEditor/src/client/reducers/responseReducer.js
@@ -1,14 +1,22 @@
-import { GET_QUERY_RESPONSE, UPDATE_CURR_RESPONSE } from '../constants/actionTypes';
+import {
+  GET_QUERY_RESPONSE,
+  UPDATE_CURR_RESPONSE,
+  CREATE_ANAGRAPH,
+  UPDATE_CURR_ANAGRAPH,
+} from '../constants/actionTypes';
 
 const initialState = {
   currResponse: null,
+  currAnagraph: null,
   responseList: [],
+  anagraphList: [],
 };
 
 const responseReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_QUERY_RESPONSE:
       return {
+        ...state,
         currResponse: action.payload,
         responseList: [...state.responseList, action.payload],
       };
@@ -16,6 +24,17 @@ const responseReducer = (state = initialState, action) => {
       return {
         ...state,
         currResponse: action.payload,
+      };
+    case CREATE_ANAGRAPH:
+      return {
+        ...state,
+        anagraphList: [...state.anagraphList, action.payload],
+        currAnagraph: action.payload,
+      };
+    case UPDATE_CURR_ANAGRAPH:
+      return {
+        ...state,
+        currAnagraph: action.payload,
       };
     default:
       return state;
