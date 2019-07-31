096a8d0f229615dcafb5cfb5d6ff78b3e2ec5e32
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-07-31 17:15:52 -0400

    fixed functionality for query and anagraph history

--- anagraphqlEditor/src/client/components/History.jsx
+++ anagraphqlEditor/src/client/components/History.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateCurrResponse, updateCurrAnagraph } from '../actions/actions';
+import { updateCurrResponse, updateCurrAnagraph, updateQuery } from '../actions/actions';
 
 const History = () => {
-  const responseList = useSelector(state => state.response.responseList);
-  const anagraphList = useSelector(state => state.response.anagraphList);
+  const responseList = useSelector(state => state.response.history.response);
+  const anagraphList = useSelector(state => state.response.history.anagraph);
+  const queryList = useSelector(state => state.response.history.query);
 
   const dispatch = useDispatch();
   const handleUpdate = (ind) => {
     dispatch(updateCurrResponse(responseList[ind]));
     dispatch(updateCurrAnagraph(anagraphList[ind]));
+    dispatch(updateQuery(queryList[ind]));
   };
   const buttons = responseList.map((cv, i) => <button id={i} type="button" onClick={e => handleUpdate(e.currentTarget.id)} />);
 
