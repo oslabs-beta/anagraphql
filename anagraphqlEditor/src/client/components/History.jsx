81becbbb43f38f8d40f4acb9623fbd4764e5cc8d
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-07-31 11:36:47 -0400

    added anagraph  obj to history buttons

--- anagraphqlEditor/src/client/components/History.jsx
+++ anagraphqlEditor/src/client/components/History.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateCurrResponse } from '../actions/actions';
+import { updateCurrResponse, updateCurrAnagraph } from '../actions/actions';
 
 const History = () => {
   const responseList = useSelector(state => state.response.responseList);
+  const anagraphList = useSelector(state => state.response.anagraphList);
+
   const dispatch = useDispatch();
-  const handleUpdate = ind => dispatch(updateCurrResponse(responseList[ind]));
+  const handleUpdate = (ind) => {
+    dispatch(updateCurrResponse(responseList[ind]));
+    dispatch(updateCurrAnagraph(anagraphList[ind]));
+  };
   const buttons = responseList.map((cv, i) => <button id={i} type="button" onClick={e => handleUpdate(e.currentTarget.id)} />);
 
   return (
-      <div className="history">
-          {buttons}
-        </div>
+    <div className="history">
+      {buttons}
+    </div>
   );
 };
 
