08bfc3e31d5b2389464fd76a3fd0e97ed87cbbd3
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-07-31 18:28:36 -0400

    removed actions not used

--- anagraphqlEditor/src/client/reducers/queryReducer.js
+++ anagraphqlEditor/src/client/reducers/queryReducer.js
@@ -1,24 +1,19 @@
 import {
-  UPDATE_QUERY, ADD_SAVED_QUERY, DELETE_SAVED_QUERY, GET_SCHEMA,
+  UPDATE_QUERY, GET_SCHEMA,
 } from '../constants/actionTypes';
 
 
-//Sets variables for inital query store
+// Sets variables for inital query store
 const initialState = {
   query: '',
-  queriesSaved: [],
   schema: null,
 };
 
-//The queryReducer function contains a seriers of conditionals that perform a specific action based on what action from our imported actions are passed in as an argument
+// The queryReducer function contains a seriers of conditionals that perform a specific action based on what action from our imported actions are passed in as an argument
 const queryReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_QUERY:
       return { ...state, query: action.payload };
-    case ADD_SAVED_QUERY:
-      return { ...state, queriesSaved: [action.payload, ...state.queriesSaved] };
-    case DELETE_SAVED_QUERY:
-      return { ...state, queriesSaved: state.queriesSaved.filter((cv, i) => i === action.payload) };
     case GET_SCHEMA:
       return { ...state, schema: action.payload };
     default:
