601f408137c414c6e6b7ca6d056b8abf7d1a9d16
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-01 15:37:03 -0400

    changed css to class from id and fixed curr marker

--- anagraphqlEditor/src/client/components/History.jsx
+++ anagraphqlEditor/src/client/components/History.jsx
@@ -11,15 +11,15 @@ const History = () => {
   const dispatch = useDispatch();
   const handleUpdate = (e) => {
     const ind = e.target.id;
-    document.querySelectorAll('#current').forEach(cv=>cv.removeAttribute('id'))
-    e.currentTarget.parentElement.parentElement.setAttribute('id', 'current')
+    document.querySelectorAll('.current').forEach(cv=>cv.removeAttribute('class'))
+    e.currentTarget.parentElement.parentElement.setAttribute('class', 'current')
     dispatch(updateCurrResponse(responseList[ind]));
     dispatch(updateCurrAnagraph(anagraphList[ind]));
     dispatch(updateQuery(queryList[ind]));
   };
   // const buttons = responseList.map((cv, i) => <button id={i} type="button" onClick={e => handleUpdate(e.currentTarget.id)} />);
   const buttons = responseList.map((cv, i) => (
-    <Animated animationIn="fadeInUp" >
+    <Animated animationIn="fadeInUp" className = {i === responseList.length - 1? "current" : ""}>
     <svg height="50" width="50">
         <circle id={i} cx="25" cy="25" r="20" stroke="black" strokeWidth="2" fill={cv["String"] === "Rule violation"? "red" : "pink"} onClick={handleUpdate} />
     </svg>
