3e2b7ebb0e16c86cbade00b254b592a56b62b265
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-01 15:11:52 -0400

    added marker functionality and color-coded error

--- anagraphqlEditor/src/client/components/History.jsx
+++ anagraphqlEditor/src/client/components/History.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateCurrResponse, updateCurrAnagraph, updateQuery } from '../actions/actions';
+import { Animated } from 'react-animated-css';
 
 const History = () => {
   const responseList = useSelector(state => state.response.history.response);
@@ -8,16 +9,21 @@ const History = () => {
   const queryList = useSelector(state => state.response.history.query);
 
   const dispatch = useDispatch();
-  const handleUpdate = (ind) => {
+  const handleUpdate = (e) => {
+    const ind = e.target.id;
+    document.querySelectorAll('#current').forEach(cv=>cv.removeAttribute('id'))
+    e.currentTarget.parentElement.parentElement.setAttribute('id', 'current')
     dispatch(updateCurrResponse(responseList[ind]));
     dispatch(updateCurrAnagraph(anagraphList[ind]));
     dispatch(updateQuery(queryList[ind]));
   };
   // const buttons = responseList.map((cv, i) => <button id={i} type="button" onClick={e => handleUpdate(e.currentTarget.id)} />);
   const buttons = responseList.map((cv, i) => (
+    <Animated animationIn="fadeInUp" >
     <svg height="50" width="50">
-      <circle id={i} cx="25" cy="25" r="20" stroke="black" strokeWidth="2" fill="pink" onClick={e => handleUpdate(e.currentTarget.id)} />
+        <circle id={i} cx="25" cy="25" r="20" stroke="black" strokeWidth="2" fill={cv["String"] === "Rule violation"? "red" : "pink"} onClick={handleUpdate} />
     </svg>
+    </Animated>
   ));
 
   return (
