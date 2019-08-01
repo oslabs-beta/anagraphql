9a1c44f09293bb96cf0d0500b423b9e3b799bb8c
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-01 10:38:04 -0400

    displaying readme markdown in /about

---
+++ anagraphqlEditor/src/client/components/CodeBlock.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+export default class CodeBlock extends React.PureComponent {
+
+
+    render() {
+        const { language, value } = this.props;
+
+        return (
+            <SyntaxHighlighter language={language}>
+                {value}
+            </SyntaxHighlighter>
+        );
+    }
+}
