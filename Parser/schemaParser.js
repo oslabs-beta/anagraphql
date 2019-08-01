0f8c3c4ac05acdc372c71a5dd20e1466af1f0857
JakobJK <jakobjk@gmail.com>
2019-07-31 20:16:24 -0400

    starting the schemaParser for the backend

---
+++ Parser/schemaParser.js
@@ -0,0 +1,17 @@
+const { getIntrospectionQuery, validateFn, introspectionFromSchema } = require('../graphqlTestServer/node_modules/graphql');
+
+module.exports = (schema) => {
+  // const types = Object.values(schema._typeMap);
+  // console.log(types);
+  // console.log(types);
+  // const sure = introspectionFromSchema(schema);
+  // console.log(JSON.stringify(sure));
+  // console.log(schema._typeMap.person._fields);
+  // console.log(schema._typeMap.types[0]._fields);
+  // function runOnObj(key) {
+  //   console.log(schema.typeMap);
+  // }
+  // for (let i = 0; i < types.length; i += 1) {
+  //   runOnObj(types[i]);
+  // }
+};
