2d3fe757012ecf00a3556f2e694a9538158f5159
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-10 23:19:56 -0400

    handling multiple rule objects & formatting

--- anagraphqlEditor/src/client/reducers/rulesReducer.js
+++ anagraphqlEditor/src/client/reducers/rulesReducer.js
@@ -1,55 +1,55 @@
 import {
-    UPDATE_CURR_RULE, SAVE_CONFIGURATION, UPDATE_NESTED_QUERIES, UPDATE_RESOLVERS, UPDATE_FIELDS
+  UPDATE_CURR_RULE, SAVE_CONFIGURATION, UPDATE_NESTED_QUERIES, UPDATE_RESOLVERS, UPDATE_FIELDS,
 } from '../constants/actionTypes';
 
 const initialState = {
-    currRule: {
-        numberOfNestedQueries: 1,
-        numberOfResolvers: 1,
-        numberOfFields: 1
-    },
-    rules: []
+  currRule: queryRules || {
+    numberOfNestedQueries: 1,
+    numberOfResolvers: 1,
+    numberOfFields: 1,
+  },
+  rules: queryRules ? [queryRules] : [],
 };
 
 const rulesReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SAVE_CONFIGURATION:
-            return {
-                ...state,
-                rules: [...state.rules, action.payload]
-            };
-        case UPDATE_CURR_RULE:
-            return {
-                ...state,
-                currRule: {...state.rules[action.payload].rules}
-            };
-        case UPDATE_NESTED_QUERIES:
-            return {
-                ...state,
-                currRule: {
-                    ...state.currRule,
-                    numberOfNestedQueries: action.payload
-                }
-            };
-        case UPDATE_FIELDS:
-            return {
-                ...state,
-                currRule: {
-                    ...state.currRule,
-                    numberOfFields: action.payload
-                }
-            };
-        case UPDATE_RESOLVERS:
-            return {
-                ...state,
-                currRule: {
-                    ...state.currRule,
-                    numberOfResolvers: action.payload
-                }
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SAVE_CONFIGURATION:
+      return {
+        ...state,
+        rules: [...state.rules, action.payload],
+      };
+    case UPDATE_CURR_RULE:
+      return {
+        ...state,
+        currRule: { ...state.rules[action.payload].rules },
+      };
+    case UPDATE_NESTED_QUERIES:
+      return {
+        ...state,
+        currRule: {
+          ...state.currRule,
+          numberOfNestedQueries: action.payload,
+        },
+      };
+    case UPDATE_FIELDS:
+      return {
+        ...state,
+        currRule: {
+          ...state.currRule,
+          numberOfFields: action.payload,
+        },
+      };
+    case UPDATE_RESOLVERS:
+      return {
+        ...state,
+        currRule: {
+          ...state.currRule,
+          numberOfResolvers: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
 };
 
 export default rulesReducer;
