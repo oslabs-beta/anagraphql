3b710b04dc05514503b9484f46cdb7eea9c70fcc
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-08-10 23:16:36 -0400

    ocd

--- renderGraphiql.js
+++ renderGraphiql.js
@@ -4,9 +4,7 @@ const fs = require('fs');
 module.exports = (rules) => {
   const content = fs.readFileSync(path.join(__dirname, 'bundle.js'), 'utf-8');
   const readMe = fs.readFileSync(path.join(__dirname, 'README.md'), 'utf-8').replace(/`/g, '\\`');
-  if (rules === undefined) {
-    rules = 'Just a string';
-  }
+
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,8 +13,10 @@ module.exports = (rules) => {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AnagraphQL</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-    <script>const readMe = \`${readMe}\`
-            const queryRules = JSON.parse(\`${JSON.stringify(rules)}\`);
+    <script>
+      const readMe = \`${readMe}\`
+      const queryRules = JSON.parse(\`${JSON.stringify(rules || null)}\`);
+      
     </script>
 </head>
 <body>
