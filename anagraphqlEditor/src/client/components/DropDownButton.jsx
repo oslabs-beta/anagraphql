c405538157ce3b85f977a06e448943553bb13076
Jacky <jackychow328@gmail.com>
2019-08-13 11:12:08 -0400

    button closes now

--- anagraphqlEditor/src/client/components/DropDownButton.jsx
+++ anagraphqlEditor/src/client/components/DropDownButton.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
 import { updateCurrRule } from '../actions/actions'
 
 const DropDownButton = () => {
     const currRules = useSelector(state => state.rules.rules);
-    console.log(currRules);
+    const [toggle, setToggle] = useState(false);
     const dispatch = useDispatch();
+    const listOfRules = currRules.map((rule, index) => (
+        <li onClick={() => clickHandler(index)}>{rule.name}</li>
+    ));
     const clickHandler = (index) => {
         dispatch(updateCurrRule(index));
+        setToggle(false);
     };
-    const listOfRules = currRules.map((rule, index) => (
-        <Dropdown.Item as="button" onClick={() => clickHandler(index)}>{rule.name}</Dropdown.Item>
-    ));
     return (
-        <div id="dropdown">
-            <DropdownButton id="dropbtn" title="Policies">
-                <div id="myDropdown" className="dropdown-content">
+        <div className="dropdown" style={{background: 'red'}}>
+            <button onClick={() => setToggle(true)}>Policies</button>
+            {toggle ? (
+                <ul>
                     {listOfRules}
-                </div>
-            </DropdownButton>
+                </ul>
+            ): (null)}
         </div>
     );
 };
